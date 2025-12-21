800447c600c63cdc2b2d908bfc58d211e6efbe82
abishek3032 <abishekyo22@gmail.com>
2025-12-21 10:22:42 +0530

    removed topic

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ dotenv.config({ quiet: true })
 app.use(express.json())
 app.use("/todo", todoRouter)
 
-console.log("TODO APP ")
-
 app.listen(process.env.PORT, () => {
     console.log(chalk.green(`http://localhost:${process.env.PORT}`))
 }
