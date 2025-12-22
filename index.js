ebe567d06e5dd8b86b31ec3e83084cd9f3f8e829
abishek3032 <abishekyo22@gmail.com>
2025-12-22 18:56:50 +0530

    added public dir

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express()
 dotenv.config({ quiet: true })
 
 app.use(express.json())
-// app.use(urlencoded({ extended: true }))
+
+app.use("/avatar",express.static("public"))
+
 app.use((req,res,next)=>{
     console.log(chalk.yellow(`${req.method} - ${req.url}`))
     next()
