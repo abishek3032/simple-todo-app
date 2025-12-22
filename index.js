9dea20620b0bcac88619d7c51c7887bdd3922df3
abishek3032 <abishekyo22@gmail.com>
2025-12-22 19:07:56 +0530

    added morgan and helmet

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+// IMPORTS
 import express, {urlencoded} from 'express'
 import dotenv from 'dotenv'
 import chalk from 'chalk'
+import morgan from 'morgan'
+import helmet from "helmet";
 
 import { todoRouter } from './routes/todo.js'
 
+// CONFIGS
 const app = express()
 dotenv.config({ quiet: true })
 
+// MIDDLEWARES
+app.use(helmet())
 app.use(express.json())
-
 app.use("/avatar",express.static("public"))
+app.use(morgan("tiny"))
 
-app.use((req,res,next)=>{
-    console.log(chalk.yellow(`${req.method} - ${req.url}`))
-    next()
-})
+// app.use((req,res,next)=>{
+//     console.log(chalk.yellow(`${req.method} - ${req.url}`))
+//     next()
+// })
 
 app.use("/todo", todoRouter)
 
