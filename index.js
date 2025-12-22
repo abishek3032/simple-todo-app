1b25aa6da5e5fd470d7bd02d839819d915ad7458
abishek3032 <abishekyo22@gmail.com>
2025-12-22 16:44:33 +0530

    Oh boy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {urlencoded} from 'express'
 import dotenv from 'dotenv'
 import chalk from 'chalk'
 
@@ -8,6 +8,12 @@ const app = express()
 dotenv.config({ quiet: true })
 
 app.use(express.json())
+// app.use(urlencoded({ extended: true }))
+app.use((req,res,next)=>{
+    console.log(chalk.yellow(`${req.method} - ${req.url}`))
+    next()
+})
+
 app.use("/todo", todoRouter)
 
 app.listen(process.env.PORT, () => {
